210e856f5fb1527f53d94ae4b8a82b5df8798a77
MdAbuNafee <nafeedgbhs@gmail.com>
2025-06-04 22:46:04 +0200

    added a created at field in model DecisionInstance

--- asynchronous_service_communication/admin.py
+++ asynchronous_service_communication/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
-from .models import DecisionInstance
-
-admin.site.register(DecisionInstance)
+# from django.contrib import admin
+# from .models import DecisionInstance
+#
+# admin.site.register(DecisionInstance)
--- asynchronous_service_communication/models.py
+++ asynchronous_service_communication/models.py
@@ -6,6 +6,7 @@ from asynchronous_service_communication import constant
 class DecisionInstance(models.Model):
     station_id = models.UUIDField()
     driver_token = models.CharField(max_length=constant.MAX_DRIVER_TOKEN_LENGTH)
+    created_at = models.DateTimeField(auto_now_add=True)
     decision = models.CharField(
         max_length=10,
     )
